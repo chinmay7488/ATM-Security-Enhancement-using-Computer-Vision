a5a13c104c00dc6f53e1abe66ad9814e99f91700
Chinmay Jain <chinmayjain8314@gmail.com>
2024-04-08 22:56:56 +0530

    Updated mysql code to Sqlite

--- Code/my_sql.py
+++ Code/my_sql.py
@@ -1,35 +1,39 @@
-import mysql.connector as mysql
+import sqlite3
+import os
+from sqlite3 import Error
 from datetime import datetime
 
 class my_sql():
     
-    def __init__(self, filename, entry_time):
-        date=None
-        mycursor=None
-        # Connecting Mysql server 
-        mydb = mysql.connect(
-                host="localhost",
-                user="root",
-                password="1234",
-                database="minor_project"
-                )
-        if(mydb):
-            print("Connection Successfull")
-        else:
-            print("Connection Failed")
-        # accessing MySQL Cursor
-        mycursor=mydb.cursor()
-        # Fetching current date and time
-        today=datetime.now()
-        # converting date to date from date and time
-        date= str(today.year)+"-"+str(today.month)+"-"+str(today.day)
+    def __init__(self):
+        if(not os.path.exists("ATMLog.db")):
+            try:
+                connection = sqlite3.connect("ATMLog.db")
+                tableCursor = connection.cursor()
+                table = "CREATE TABLE atm_machine(Person_Photo BLOB NOT NULL, Date DATETIME NOT NULL, Entry_Time DATETIME NOT NULL);"
+                tableCursor.execute(table)
+            except sqlite3.Error as e:
+                print(e)
+            finally:
+                if connection:
+                    connection.close()
 
-        # opening image as binary and reading it
-        with open(filename, "rb") as file:
-             photo = file.read()
-        # Mysql command to insert value and executing it
-        SqlStatement="INSERT INTO atm_machine(photo, date, entry_time) VALUES (%s,%s,%s)"
-        mycursor.execute(SqlStatement, (photo, date, entry_time))
-        # Commiting changes
-        mydb.commit()
+    def Insert_data(self, filename, entry_time):
+        try:
+            date=None
+            tableCursor=None
+            connection = sqlite3.connect("ATMLog.db")
+            tableCursor=connection.cursor()
+            today=datetime.now()
+            date= str(today.year)+"-"+str(today.month)+"-"+str(today.day)
 
+            with open(filename, "rb") as file:
+                photo = file.read()
+            SqlStatement="INSERT INTO atm_machine VALUES(?, ?, ?)"
+            tableCursor.execute(SqlStatement, (photo, date, entry_time))
+            connection.commit()
+        except Exception as ex:
+            print(ex)
+        finally:
+                if connection:
+                    connection.close()
